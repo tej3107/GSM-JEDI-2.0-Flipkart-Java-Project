5d74f720d1b9bb5a73aa43ff3428a783358978d5
MdJunaidMahmood <mdjunaidmahmood@gmail.com>
2023-06-20 22:33:41 +0530

    Added Skeleton for Beans and Services

---
+++ GMS_JEDI_2_Flipkart_Project_Java/src/com/flipkart/bean/User.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.flipkart.bean;
+
+/**
+ * 
+ */
+public class User {
+	private String userName;
+	private String password;
+	private int roleId;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public int getRoleId() {
+		return roleId;
+	}
+	
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+}
---
+++ GMS_JEDI_2_Flipkart_Project_Java/src/com/flipkart/client/AppClient.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.client;
+
+/**
+ * 
+ */
+public class AppClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
